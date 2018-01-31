cdb2b6b54cf45153b38ce5f4543091a9c6b18f88
khan <khanzu@ece.neu.edu>
2018-01-30 19:27:12 -0500

    fixed RBF function, runs are in place.
    Signed-off-by: khan <khanzu@ece.neu.edu>

--- tfaTorch.py
+++ tfaTorch.py
@@ -5,11 +5,13 @@ from torch.autograd import Variable
 from torch.nn import Parameter
 import probtorch
 import scipy.io as sio
+import numpy as np
 
 CUDA =torch.cuda.is_available()
 NUM_EPOCH = 10 #placeholder values
 NUM_BATCH = 45 #placeholder values
 NUM_SAMPLES = 10 #placeholder values
+LEARNING_RATE = 0.1
 
 dataset = sio.loadmat('subj1.mat')
 voxelActivations = torch.Tensor(dataset['data']) #data
@@ -20,8 +22,10 @@ NUM_VOXELS = voxelLocations.shape[0]     #no. of voxels
 NUM_FACTORS = 50             #no. of factors
 
 
-brainCenter = torch.mean(voxelActivations,0)  #c
-sourceCenterVariance = torch.Tensor([1,1,1]) #kappa_u
+c = torch.mean(voxelLocations,0)#c
+brainCenter = c.unsqueeze(0)
+# brainCenter = torch.Tensor([[c[0]],[c[1]],[c[2]]])
+sourceCenterVariance = torch.Tensor([1,1,1]).unsqueeze(0) #kappa_u
 sourceWeightVariance = 2 #kappa_w
 sourceWidthVariance = 3
 voxelNoise = 0.1 #sigma2_y
@@ -35,50 +39,67 @@ class Encoder(nn.Module):
         super(self.__class__, self).__init__()
         self.MeanWeight = Parameter(torch.randn((NUM_TRs,NUM_FACTORS)))
         self.SigmaWeight = Parameter(torch.randn((NUM_TRs,NUM_FACTORS)))
-        self.MeanFactorCenter = Parameter(torch.randn((3,NUM_FACTORS)))
-        self.SigmaFactorCenter = Parameter(torch.randn((3,NUM_FACTORS)))
+        self.MeanFactorCenter = Parameter(torch.randn((NUM_FACTORS,3)))
+        self.SigmaFactorCenter = Parameter(torch.randn((NUM_FACTORS,3)))
         self.MeanFactorWidth = Parameter(torch.randn((NUM_FACTORS)))
         self.SigmaFactorWidth = Parameter(torch.randn((NUM_FACTORS)))
 
-    def forward(self):
+    def forward(self,NUM_SAMPLES = NUM_SAMPLES):
 
         q = probtorch.Trace()
-
-        Weights = q.normal(self.MeanWeight, self.SigmaWeight, name = 'Weights') #W
-        FactorCenters = q.normal(self.MeanFactorCenter,self.MeanFactorCenter, name = 'FactorCenters') #M
-        FactorWidths = q.normal(self.MeanFactorWidth,self.SigmaFactorWidth,name = 'FactorWidths') #L
+        MeanWeight = self.MeanWeight.expand(NUM_SAMPLES,NUM_TRs,NUM_FACTORS)
+        SigmaWeight = self.SigmaWeight.expand(NUM_SAMPLES,NUM_TRs,NUM_FACTORS)
+        MeanFactorCenter = self.MeanFactorCenter.expand(NUM_SAMPLES,NUM_FACTORS,3)
+        SigmaFactorCenter = self.SigmaFactorCenter.expand(NUM_SAMPLES,NUM_FACTORS,3)
+        MeanFactorWidth = self.MeanFactorWidth.expand(NUM_SAMPLES,NUM_FACTORS)
+        SigmaFactorWidth = self.SigmaFactorWidth.expand(NUM_SAMPLES,NUM_FACTORS)
+        Weights = q.normal(MeanWeight, SigmaWeight, name = 'Weights') #W
+        FactorCenters = q.normal(MeanFactorCenter,SigmaFactorCenter, name = 'FactorCenters') #M
+        FactorWidths = q.normal(MeanFactorWidth,SigmaFactorWidth,name = 'FactorWidths') #L
 
 
         return q
 
 class Decoder(nn.Module):
-    def __init__(self,NUM_TRs,NUM_FACTORS,NUM_VOXELS):
-        self.MeanWeight = torch.zeros((NUM_TRs, NUM_FACTORS))
-        self.SigmaWeight = sourceWeightVariance*torch.ones((NUM_TRs,NUM_FACTORS))
-        self.MeanFactorCenter = (brainCenter.expand(3,NUM_FACTORS))*torch.ones((3,NUM_FACTORS))  #c is center of 3D brain image
-        self.SigmaFactorCenter = (sourceCenterVariance.expand(3,NUM_FACTORS))*torch.ones((3,NUM_FACTORS)) #s
-        self.MeanFactorWidth = torch.ones((NUM_FACTORS))
-        self.SigmaFactorWidth = sourceWidthVariance*torch.ones((NUM_FACTORS))
-        self.Snoise = voxelNoise*torch.ones(NUM_VOXELS)
+    def __init__(self,NUM_TRs =NUM_TRs,NUM_FACTORS = NUM_FACTORS,NUM_VOXELS = NUM_VOXELS):
+        super(self.__class__, self).__init__()
+        self.MeanWeight = Parameter(torch.zeros((NUM_TRs, NUM_FACTORS)))
+        self.SigmaWeight = Parameter(sourceWeightVariance*torch.ones((NUM_TRs,NUM_FACTORS)))
+        self.MeanFactorCenter = Parameter((brainCenter.expand(NUM_FACTORS,3))*torch.ones((NUM_FACTORS,3))) #c is center of 3D brain image
+        self.SigmaFactorCenter = Parameter((sourceCenterVariance.expand(NUM_FACTORS,3))*
+                                          torch.ones((NUM_FACTORS,3))) #s
+        self.MeanFactorWidth = Parameter(torch.ones((NUM_FACTORS)))
+        self.SigmaFactorWidth = Parameter(sourceWidthVariance*torch.ones((NUM_FACTORS)))
+        self.Snoise = Parameter(voxelNoise*torch.ones(NUM_TRs,NUM_VOXELS))
 
     def forward(self,data = voxelActivations,R = voxelLocations,q=None):
         p = probtorch.Trace()
-        Weights = p.normal(self.MeanWeight,self.SigmaWeightw,value = q['Weights'],name = 'Weights')
+        Weights = p.normal(self.MeanWeight,self.SigmaWeight,value = q['Weights'],name = 'Weights')
         FactorCenters = p.normal(self.MeanFactorCenter,self.SigmaFactorCenter,value = q['FactorCenters'],
                                  name = 'FactorCenters')
         FactorWidths = p.normal(self.MeanFactorWidth,self.SigmaFactorWidth,value = q['FactorWidths'],
                                 name = 'FactorWidths')
         Factors = Function_RBF(R,FactorCenters,FactorWidths)
-        Yhat = p.normal(torch.matmul(Weights,Factors),self.Snoise,name = 'Yhat')
-        p.loss(((Yhat - data).T)*(Yhat-data),name='y')
+        data = p.normal(torch.matmul(Weights,Factors),self.Snoise.unsqueeze(0).expand(NUM_SAMPLES,NUM_TRs,NUM_VOXELS),value = data,name = 'Y')
+        # p.loss(((Yhat - data).T)*(Yhat-data),name='y') ##do we need this?
 
         return p
 
-def Function_RBF(locations, centers, distances):
-    return torch.exp(-(locations + -centers).norm()**2 / torch.exp(distances))
+#
+# 3 x V
+# 3 x K
+#
+# 3 x V x 1
+# 3 x 1 x K
+# 1 x 1 x K
 
-def elbo(q,p):
-    return probtorch.objectives.montecarlo.kl(q,p)   #negative taken later
+def Function_RBF(locations, centers, distances):
+    locations = locations.unsqueeze(0)
+    locations = locations.expand(10,NUM_VOXELS,3)
+    return torch.exp((((locations.unsqueeze(1) - centers.unsqueeze(2))**2).sum(3))/(-torch.exp(distances.unsqueeze(2))))
+
+def elbo(q,p,NUM_SAMPLES = 10):
+    return probtorch.objectives.montecarlo.kl(q,p,sample_dim=NUM_SAMPLES)   #negative taken later
 
 enc = Encoder()
 dec = Decoder()
@@ -89,70 +110,44 @@ if CUDA:
 
 optimizer = torch.optim.Adam(list(enc.parameters()),lr= LEARNING_RATE)
 
-def train(data,enc,dec,optimizer):
-    epoch_elbo = 0
+def train(data,R,enc,dec,optimizer,num_steps):
     enc.train()
     dec.train()
-    N = 0
-    for counter,(voxacts,voxlocs) in enumerate(data):
-        N+=NUM_BATCH
-        if CUDA:
-            voxacts = voxacts.cuda()
-            voxlocs = voxlocs.cuda()
-        voxacts = Variable(voxacts)
-        voxlocs = Variable(voxlocs)
+    losses = np.zeros(num_steps)
+    for n in range(num_steps):
         optimizer.zero_grad()
         q = enc()
-        p = dec(data = voxacts,R = voxlocs,q = q)
-        loss = -elbo(q,p)
+        if CUDA:
+            data = data.cuda()
+            R = R.cuda()
+        data = Variable(data)
+        R = Variable(R)
+        p = dec(data = data,R =R, q = q)
+        loss = -elbo(p, q)
         loss.backward()
         optimizer.step()
         if CUDA:
             loss = loss.cpu()
-        epoch_elbo -= loss.data.numpy()[0]
-
-        return epoch_elbo/N
-
-def test(data,enc,dec):
-    enc.eval()
-    dec.eval()
-
-    epoch_elbo = 0
-    N = 0
-
-    for counter,(voxacts,voxlocs) in data:
-        N+=NUM_BATCH
-        if CUDA:
-            voxacts = voxacts.cuda()
-            voxlocs = voxlocs.cuda()
-
-        voxacts = Variable(voxacts)
-        voxlocs = Variable(voxlocs)
-        q = enc()
-        p = dec(data = voxacts,R = voxlocs,q =q)
-        batch_elbo = elbo(q,p)
-
-        if CUDA:
-            batch_elbo = batch_elbo.cpu()
-
-        epoch_elbo += batch_elbo.data.numpy()[0]
-
-    return epoch_elbo/N
+        losses[n] = loss.data.numpy()[0]
+    return losses
 
 
+losses = train(voxelActivations,voxelLocations,enc,dec,optimizer,num_steps=10)
 
-
-for e in range(NUM_EPOCH):
-    train_elbo = train(train_data,enc,dec)
-    #test_elbo = test(test_data,enc,dec)
-
-    print('[Epoch %d] Train: ELBO %.4e Test: ELBO %.4e' % (e, train_elbo))
+#
+# for e in range(NUM_EPOCH):
+#     train_data = list(voxelActivations,voxelLocations)
+#     train_elbo = train(train_data,enc,dec)
+#     #test_elbo = test(test_data,enc,dec)
+#
+#     print('[Epoch %d] Train: ELBO %.4e Test: ELBO %.4e' % (e, train_elbo))
 
 if CUDA:
     q = enc()
     W = q['Weights'].value.data.cpu().numpy()
     M = q['FactorCenters'].value.data.cpu().numpy()
     L = q['FactorWidths'].value.data.cpud().numpy()
+
 
 
 
