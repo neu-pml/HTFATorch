2485fab19fbda55f6a09a9140394f747dab45a02
Eli Sennesh <esennesh@cs.technion.ac.il>
2018-02-01 23:32:23 -0500

    tfa: lint the heck out of it
    Signed-off-by: Eli Sennesh <esennesh@cs.technion.ac.il>

--- tfa/tfa.py
+++ tfa/tfa.py
@@ -1,35 +1,45 @@
 #!/usr/bin/env python
+"""Perform plain topographic factor analysis on a given fMRI data file."""
 
-# import dependencies
+__author__ = 'Eli Sennesh'
+__email__ = 'e.sennesh@northeastern.edu'
+
 import argparse
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+import time
 import numpy as np
 import probtorch
 import scipy.io as sio
 import scipy.optimize
-import time
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
 from torch.nn import Parameter
+
+try:
+    import matplotlib
+    matplotlib.use('TkAgg')
+finally:
+    import matplotlib.pyplot as plt
 
 # check the availability of CUDA
 CUDA = torch.cuda.is_available()
 
 # placeholder values for hyperparameters
 LEARNING_RATE = 1e-4
-NUM_FACTORS   = 50
-NUM_SAMPLES   = 10
-SOURCE_WEIGHT_STD_DEV    = np.sqrt(2.0)
+NUM_FACTORS = 50
+NUM_SAMPLES = 10
+SOURCE_WEIGHT_STD_DEV = np.sqrt(2.0)
 SOURCE_LOG_WIDTH_STD_DEV = np.sqrt(3.0)
 VOXEL_NOISE = 0.1
+
+EPOCH_MSG = '[Epoch %d] (%ds) Posterior free-energy %.8e, Joint KL divergence %.8e'
 
 # locations: V x 3
 # centers: S x K x 3
 # log_widths: S x K
-def radial_basis(locations, centers, log_widths, num_voxels, num_samples=NUM_SAMPLES):
+def radial_basis(locations, centers, log_widths, num_voxels,
+                 num_samples=NUM_SAMPLES):
+    """The radial basis function used as the shape for the factors"""
     # V x 3 -> S x 1 x V x 3
     locations = locations.expand(num_samples, num_voxels, 3).unsqueeze(1)
     # S x K x 3 -> S x K x 1 x 3
@@ -40,77 +50,128 @@ def radial_basis(locations, centers, log_widths, num_voxels, num_samples=NUM_SAM
     log_widths = log_widths.unsqueeze(2)
     return torch.exp(-delta2s.sum(3) / torch.exp(log_widths))
 
-def free_energy(q, p, num_samples=NUM_SAMPLES):
-    # Remember: the Evidence Lower Bound is the negative free-energy
-    return -probtorch.objectives.montecarlo.elbo(q, p, sample_dim=0)
+def free_energy(q, p):
+    """Calculate the free-energy (negative of the evidence lower bound)"""
+    return -probtorch.objectives.montecarlo.elbo(q, p)
 
-def kl_divergence(q, p, num_samples=NUM_SAMPLES):
-    return probtorch.objectives.montecarlo.kl(q, p, sample_dim=0)
+def kl_divergence(q, p):
+    """Calculate the KL divergence from the joint distribution"""
+    return probtorch.objectives.montecarlo.kl(q, p)
 
 class TFAEncoder(nn.Module):
+    """Variational guide for topographic factor analysis"""
     def __init__(self, num_times, num_factors=NUM_FACTORS):
         super(self.__class__, self).__init__()
         self._num_times = num_times
         self._num_factors = num_factors
 
-        self._mean_weight = Parameter(torch.randn((self._num_times, self._num_factors)))
-        self._weight_std_dev = Parameter(torch.sqrt(torch.rand((self._num_times, self._num_factors))))
+        self._mean_weight = Parameter(torch.randn(
+            (self._num_times, self._num_factors)
+        ))
+        self._weight_std_dev = Parameter(torch.sqrt(torch.rand(
+            (self._num_times, self._num_factors)
+        )))
 
-        self._mean_factor_center = Parameter(torch.randn((self._num_factors, 3)))
-        self._factor_center_std_dev = Parameter(torch.sqrt(torch.rand((self._num_factors, 3))))
+        self._mean_factor_center = Parameter(torch.randn(
+            (self._num_factors, 3)
+        ))
+        self._factor_center_std_dev = Parameter(torch.sqrt(torch.rand(
+            (self._num_factors, 3)
+        )))
 
-        self._mean_factor_log_width = Parameter(torch.randn((self._num_factors)))
-        self._factor_log_width_std_dev = Parameter(torch.sqrt(torch.rand((self._num_factors))))
+        self._mean_factor_log_width = Parameter(torch.randn(
+            (self._num_factors)
+        ))
+        self._factor_log_width_std_dev = Parameter(torch.sqrt(torch.rand(
+            (self._num_factors)
+        )))
 
-    def forward(self, num_samples = NUM_SAMPLES):
+    def forward(self, num_samples=NUM_SAMPLES):
         q = probtorch.Trace()
 
-        mean_weight = self._mean_weight.expand(num_samples, self._num_times, self._num_factors)
-        weight_std_dev = self._weight_std_dev.expand(num_samples, self._num_times, self._num_factors)
+        mean_weight = self._mean_weight.expand(num_samples, self._num_times,
+                                               self._num_factors)
+        weight_std_dev = self._weight_std_dev.expand(num_samples,
+                                                     self._num_times,
+                                                     self._num_factors)
 
-        mean_factor_center = self._mean_factor_center.expand(num_samples, self._num_factors, 3)
-        factor_center_std_dev = self._factor_center_std_dev.expand(num_samples, self._num_factors, 3)
+        mean_factor_center = self._mean_factor_center.expand(num_samples,
+                                                             self._num_factors,
+                                                             3)
+        factor_center_std_dev = self._factor_center_std_dev.expand(
+            num_samples, self._num_factors, 3
+        )
 
-        mean_factor_log_width = self._mean_factor_log_width.expand(num_samples, self._num_factors)
-        factor_log_width_std_dev = self._factor_log_width_std_dev.expand(num_samples, self._num_factors)
+        mean_factor_log_width = self._mean_factor_log_width.expand(
+            num_samples, self._num_factors
+        )
+        factor_log_width_std_dev = self._factor_log_width_std_dev.expand(
+            num_samples, self._num_factors
+        )
 
-        weights = q.normal(mean_weight, weight_std_dev, name='Weights')
+        q.normal(mean_weight, weight_std_dev, name='Weights')
 
-        factor_centers = q.normal(mean_factor_center, factor_center_std_dev, name='FactorCenters')
-        factor_log_widths = q.normal(mean_factor_log_width, factor_log_width_std_dev, name='FactorLogWidths')
+        q.normal(mean_factor_center, factor_center_std_dev,
+                 name='FactorCenters')
+        q.normal(mean_factor_log_width, factor_log_width_std_dev,
+                 name='FactorLogWidths')
 
         return q
 
 class TFADecoder(nn.Module):
-    def __init__(self, brain_center, brain_center_std_dev, num_times, num_voxels, num_factors=NUM_FACTORS):
+    """Generative model for topographic factor analysis"""
+    def __init__(self, brain_center, brain_center_std_dev, num_times,
+                 num_voxels, num_factors=NUM_FACTORS):
         super(self.__class__, self).__init__()
         self._num_times = num_times
         self._num_factors = num_factors
         self._num_voxels = num_voxels
 
-        self._mean_weight = Variable(torch.zeros((self._num_times, self._num_factors)))
-        self._weight_std_dev = Variable(SOURCE_WEIGHT_STD_DEV * torch.ones((self._num_times, self._num_factors)))
+        self._mean_weight = Variable(torch.zeros(
+            (self._num_times, self._num_factors)
+        ))
+        self._weight_std_dev = Variable(SOURCE_WEIGHT_STD_DEV *
+                                        torch.ones((self._num_times,
+                                                    self._num_factors)))
 
-        self._mean_factor_center = Variable(brain_center.expand(self._num_factors, 3) * torch.ones((self._num_factors, 3)))
-        self._factor_center_std_dev = Variable(brain_center_std_dev.expand(self._num_factors, 3) * torch.ones((self._num_factors, 3)))
+        self._mean_factor_center = Variable(
+            brain_center.expand(self._num_factors, 3) *
+            torch.ones((self._num_factors, 3))
+        )
+        self._factor_center_std_dev = Variable(
+            brain_center_std_dev.expand(self._num_factors, 3) *
+            torch.ones((self._num_factors, 3))
+        )
 
         self._mean_factor_log_width = Variable(torch.ones((self._num_factors)))
-        self._factor_log_width_std_dev = Variable(SOURCE_LOG_WIDTH_STD_DEV * torch.ones((self._num_factors)))
+        self._factor_log_width_std_dev = Variable(
+            SOURCE_LOG_WIDTH_STD_DEV * torch.ones((self._num_factors))
+        )
 
         self._voxel_noise = Variable(VOXEL_NOISE * torch.ones(self._num_times, self._num_voxels))
 
     def forward(self, activations, locations, q=None):
         p = probtorch.Trace()
 
-        weights = p.normal(self._mean_weight, self._weight_std_dev, value=q['Weights'], name='Weights')
-        factor_centers = p.normal(self._mean_factor_center, self._factor_center_std_dev, value=q['FactorCenters'], name='FactorCenters')
-        factor_log_widths = p.normal(self._mean_factor_log_width, self._factor_log_width_std_dev, value=q['FactorLogWidths'], name='FactorLogWidths')
-        factors = radial_basis(locations, factor_centers, factor_log_widths, num_voxels = self._num_voxels)
-        observations = p.normal(torch.matmul(weights, factors), self._voxel_noise, value=activations, name='Y')
+        weights = p.normal(self._mean_weight, self._weight_std_dev,
+                           value=q['Weights'], name='Weights')
+        factor_centers = p.normal(self._mean_factor_center,
+                                  self._factor_center_std_dev,
+                                  value=q['FactorCenters'],
+                                  name='FactorCenters')
+        factor_log_widths = p.normal(self._mean_factor_log_width,
+                                     self._factor_log_width_std_dev,
+                                     value=q['FactorLogWidths'],
+                                     name='FactorLogWidths')
+        factors = radial_basis(locations, factor_centers, factor_log_widths,
+                               num_voxels=self._num_voxels)
+        p.normal(torch.matmul(weights, factors), self._voxel_noise,
+                 value=activations, name='Y')
 
         return p
 
 class TopographicalFactorAnalysis:
+    """Overall container for a run of TFA"""
     def __init__(self, data_file):
         dataset = sio.loadmat(data_file)
         # pull out the voxel activations and locations
@@ -120,22 +181,27 @@ class TopographicalFactorAnalysis:
         # This could be a huge file.  Close it
         del dataset
 
-        # Pull out relevant dimensions: the number of times-of-recording, and the number of voxels in each timewise "slice"
+        # Pull out relevant dimensions: the number of times-of-recording, and
+        # the number of voxels in each timewise "slice"
         self.num_times = self.voxel_activations.shape[0]
         self.num_voxels = self.voxel_activations.shape[1]
 
         # Estimate further hyperparameters from the dataset
         self.brain_center = torch.mean(self.voxel_locations, 0).unsqueeze(0)
-        self.brain_center_std_dev = torch.sqrt(10 * torch.var(self.voxel_locations, 0).unsqueeze(0))
+        self.brain_center_std_dev = torch.sqrt(
+            10 * torch.var(self.voxel_locations, 0).unsqueeze(0)
+        )
 
         self.enc = TFAEncoder(self.num_times)
-        self.dec = TFADecoder(self.brain_center, self.brain_center_std_dev, self.num_times, self.num_voxels)
+        self.dec = TFADecoder(self.brain_center, self.brain_center_std_dev,
+                              self.num_times, self.num_voxels)
 
         if CUDA:
             self.enc.cuda()
             self.dec.cuda()
 
     def train(self, num_steps=10):
+        """Optimize the variational guide to reflect the data for `num_steps`"""
         activations = Variable(self.voxel_activations)
         locations = Variable(self.voxel_locations)
         optimizer = torch.optim.Adam(list(self.enc.parameters()), lr=LEARNING_RATE)
@@ -169,12 +235,13 @@ class TopographicalFactorAnalysis:
             kls[n] = kl.data.numpy()[0]
 
             end = time.time()
-            print('[Epoch %d] (%ds) Posterior free-energy %.8e, Joint KL divergence %.8e' % (n + 1, end - start, free_energy_n, kl))
+            print(EPOCH_MSG % (n + 1, end - start, free_energy_n, kl))
 
-        losses = np.vstack([free_energies, kls])
-        return losses
+        self.losses = np.vstack([free_energies, kls])
+        return self.losses
 
     def results(self):
+        """Return the inferred parameters"""
         q = self.enc()
         if CUDA:
             q['Weights'].value.data.cpu()
@@ -185,13 +252,19 @@ class TopographicalFactorAnalysis:
         factor_centers = q['FactorCenters'].value.data.numpy()
         factor_log_widths = q['FactorLogWidths'].value.data.numpy()
 
-        return {'weights': weights, 'factor_centers': factor_centers, 'factor_log_widths': factor_log_widths}
+        result = {
+            'weights': weights,
+            'factor_centers': factor_centers,
+            'factor_log_widths': factor_log_widths
+        }
+        return result
 
 parser = argparse.ArgumentParser(description='Topographical factor analysis for fMRI data')
 parser.add_argument('data_file', type=str, help='fMRI filename')
 parser.add_argument('--steps', type=int, default=100, help='Number of optimization steps')
 
 def linear(x, m, b):
+    """Your basic linear function f(x) = mx+b"""
     return m * x + b
 
 if __name__ == '__main__':
@@ -215,8 +288,8 @@ if __name__ == '__main__':
 
     kl_fig = plt.figure(figsize=(10, 10))
 
-    plt.plot(epochs, losses[1,:], 'r.', label='Data')
-    parameters, pcov = scipy.optimize.curve_fit(linear, epochs, losses[1,:])
+    plt.plot(epochs, losses[1, :], 'r.', label='Data')
+    parameters, pcov = scipy.optimize.curve_fit(linear, epochs, losses[1, :])
     plt.plot(epochs, linear(epochs, *parameters), 'r', label="Fit")
     plt.legend()
 
