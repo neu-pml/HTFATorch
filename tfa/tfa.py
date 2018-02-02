e9abc9e0a45e78726b0f01e83baf577d9f165f2b
esennesh <elisennesh@gmail.com>
2018-02-02 16:37:57 -0500

    tfa: move the plotting code into a procedure
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- tfa/tfa.py
+++ tfa/tfa.py
@@ -271,11 +271,6 @@ class TopographicalFactorAnalysis:
         }
         return result
 
-parser = argparse.ArgumentParser(description='Topographical factor analysis for fMRI data')
-parser.add_argument('data_file', type=str, help='fMRI filename')
-parser.add_argument('--steps', type=int, default=100, help='Number of optimization steps')
-parser.add_argument('--log', action='store_true', help='Whether to log optimization')
-
 def linear(x, m, b):
     """Your basic linear function f(x) = mx+b"""
     return m * x + b
@@ -288,18 +283,7 @@ def logistic(x, a, b, c):
     """Your basic logistic function"""
     return a * (1 + np.exp(-b * (np.array(x) - c)))
 
-if __name__ == '__main__':
-    args = parser.parse_args()
-    if args.log:
-        level = logging.INFO
-    else:
-        level = logging.WARNING
-    logging.basicConfig(format='%(asctime)s %(message)s',
-                        datefmt='%m/%d/%Y %H:%M:%S',
-                        level=level)
-    tfa = TopographicalFactorAnalysis(args.data_file)
-    losses = tfa.train(num_steps=args.steps, log_optimization=args.log)
-
+def plot_losses(losses):
     epochs = range(losses.shape[1])
 
     free_energy_fig = plt.figure(figsize=(10, 10))
@@ -363,3 +347,23 @@ if __name__ == '__main__':
     kl_fig.axes[0].set_ylabel('KL divergence (nats)')
 
     plt.show()
+
+parser = argparse.ArgumentParser(description='Topographical factor analysis for fMRI data')
+parser.add_argument('data_file', type=str, help='fMRI filename')
+parser.add_argument('--steps', type=int, default=100, help='Number of optimization steps')
+parser.add_argument('--log', action='store_true', help='Whether to log optimization')
+
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.log:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(format='%(asctime)s %(message)s',
+                        datefmt='%m/%d/%Y %H:%M:%S',
+                        level=level)
+    tfa = TopographicalFactorAnalysis(args.data_file)
+    losses = tfa.train(num_steps=args.steps, log_optimization=args.log)
+    plot_losses(losses)
