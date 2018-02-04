b853f1f1b8f6d51ff7005662af7238e3cd4df706
esennesh <elisennesh@gmail.com>
2018-02-04 15:53:26 -0500

    tfa: eliminate floating-point overflows in curve-fit functions
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- tfa/tfa.py
+++ tfa/tfa.py
@@ -303,11 +303,11 @@ def linear(x, m, b):
 
 def exponential(x, a, b, c):
     """Your basic exponential decay function"""
-    return a * np.exp(-b * np.array(x)) - c
+    return np.array((a * torch.exp(-b * torch.DoubleTensor(x)) - c).tolist())
 
 def logistic(x, a, b, c):
     """Your basic logistic function"""
-    return a * (1 + np.exp(-b * (np.array(x) - c)))
+    return np.array((a * (1 + torch.exp(-b * (torch.DoubleTensor(x) - c)))).tolist())
 
 def plot_losses(losses):
     epochs = range(losses.shape[1])
