30f9a17308ead60aee7564b577a3c13836e2319f
esennesh <elisennesh@gmail.com>
2018-02-04 22:13:44 -0500

    utils.py: factor out utils.py
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- tfa/tfa.py
+++ tfa/tfa.py
@@ -10,18 +10,13 @@ import logging
 import numpy as np
 import probtorch
 import scipy.io as sio
-import scipy.optimize
 import torch
 import torch.distributions as dists
 from torch.autograd import Variable
 import torch.nn as nn
 from torch.nn import Parameter
 
-try:
-    import matplotlib
-    matplotlib.use('TkAgg')
-finally:
-    import matplotlib.pyplot as plt
+import utils
 
 # check the availability of CUDA
 CUDA = torch.cuda.is_available()
@@ -297,89 +292,10 @@ class TopographicalFactorAnalysis:
         }
         return result
 
-def linear(x, m, b):
-    """Your basic linear function f(x) = mx+b"""
-    return m * x + b
-
-def exponential(x, a, b, c):
-    """Your basic exponential decay function"""
-    return np.array((a * torch.exp(-b * torch.DoubleTensor(x)) - c).tolist())
-
-def logistic(x, a, b, c):
-    """Your basic logistic function"""
-    return np.array((a * (1 + torch.exp(-b * (torch.DoubleTensor(x) - c)))).tolist())
-
-def plot_losses(losses):
-    epochs = range(losses.shape[1])
-
-    free_energy_fig = plt.figure(figsize=(10, 10))
-
-    plt.plot(epochs, losses[0,:], 'b.', label='Data')
-    try:
-        parameters, pcov = scipy.optimize.curve_fit(logistic, epochs, losses[0,:])
-        func = logistic
-        fit = 'Logistic'
-    except RuntimeError:
-        logging.warn("Falling back to exponential curve for free-energy figure")
-        try:
-            parameters, pcov = scipy.optimize.curve_fit(exponential,
-                                                        epochs,
-                                                        losses[0,:])
-            func = exponential
-            fit = 'Exponential'
-        except RuntimeError:
-            logging.warn("Falling back to linear curve for free-energy figure")
-            parameters, pcov = scipy.optimize.curve_fit(linear,
-                                                        epochs,
-                                                        losses[0,:])
-            func = linear
-            fit = 'Linear'
-    plt.plot(epochs, func(epochs, *parameters), 'b', label=fit + " Fit")
-    plt.legend()
-
-    free_energy_fig.tight_layout()
-    plt.title('Free-energy / -ELBO change over training')
-    free_energy_fig.axes[0].set_xlabel('Epoch')
-    free_energy_fig.axes[0].set_ylabel('Free-energy / -ELBO (nats)')
-
-    kl_fig = plt.figure(figsize=(10, 10))
-
-    plt.plot(epochs, losses[1, :], 'r.', label='Data')
-    try:
-        parameters, pcov = scipy.optimize.curve_fit(logistic, epochs, losses[1,:])
-        func = logistic
-        fit = 'Logistic'
-    except RuntimeError:
-        logging.warn("Falling back to exponential curve for KL divergence figure")
-        try:
-            parameters, pcov = scipy.optimize.curve_fit(exponential,
-                                                        epochs,
-                                                        losses[1,:])
-            func = exponential
-            fit = 'Exponential'
-        except RuntimeError:
-            logging.warn("Falling back to linear curve for KL divergence figure")
-            parameters, pcov = scipy.optimize.curve_fit(linear,
-                                                        epochs,
-                                                        losses[1,:])
-            func = linear
-            fit = 'Linear'
-    plt.plot(epochs, func(epochs, *parameters), 'r', label=fit + " Fit")
-    plt.legend()
-
-    kl_fig.tight_layout()
-    plt.title('KL divergence change over training')
-    kl_fig.axes[0].set_xlabel('Epoch')
-    kl_fig.axes[0].set_ylabel('KL divergence (nats)')
-
-    plt.show()
-
 parser = argparse.ArgumentParser(description='Topographical factor analysis for fMRI data')
 parser.add_argument('data_file', type=str, help='fMRI filename')
 parser.add_argument('--steps', type=int, default=100, help='Number of optimization steps')
 parser.add_argument('--log', action='store_true', help='Whether to log optimization')
-
-
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -392,4 +308,4 @@ if __name__ == '__main__':
                         level=level)
     tfa = TopographicalFactorAnalysis(args.data_file)
     losses = tfa.train(num_steps=args.steps, log_optimization=args.log)
-    plot_losses(losses)
+    utils.plot_losses(losses)
