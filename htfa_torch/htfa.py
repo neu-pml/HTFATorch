63d53a656e39843ede0e2d007359bd16aa68c8ff
Zulqarnain Khan <zqkhan>
2018-02-28 21:26:26 -0500

    outline of encoder added.

--- htfa_torch/htfa.py
+++ htfa_torch/htfa.py
@@ -5,11 +5,44 @@ import torch.nn as nn
 
 class HTFAEnconder(nn.module):
     """variational guide for HTFA"""
-    def __init__(self):
+    def __init__(self,num_factors,num_subjects):
+        self.mean_mean_weight = Parameter(torch.zeros(num_factors,))
+        self.std_mean_weight = Parameter(torch.rand(num_factors,))
+        self.mean_std_weight = Parameter(torch.zeros(num_factors,))
+        self.std_std_weight = Parameter(torch.rand(num_factors,))
+        
+        self.mean_mean_factor = Parameter(torch.zeros(num_factors,3))
+        self.std_mean_factor = Parameter(torch.rand(num_factors,3))
+        self.mean_width_factor = Parameter(torch.zeros(num_factors,3))
+        self.std_width_factor = Parameter(torch.rand(num_factors,3))
+        
+        self.mean_weight = Parameter(torch.zeros(num_factors,num_subjects))
+        self.std_weight = Parameter(torch.rand(num_factors,num_subjects))
+        
+        
+        
         
     def forward(self):
+        q = probtorch.Trace()
+        mean_weight = self.mean_weight
+        std_weight =  self.std_weight
+        mean_mean_factor = self.mean_mean_factor
+        std_mean_factor = self.std_mean_factor
+        mean_width_factor = self.mean_width_factor
+        std_width_factor = self.std_width_factor
+        
+        q.normal(mean_weight.expand(num_times,num_factors,num_subjects),
+                         std_weight.expand(num_times,num_factors,num_subjects),
+                         name='weights')
+        q.normal(mean_mean_factor.expand(num_factors,3,num_subjects),
+                              std_mean_factor.expand(num_factors,3,num_subjects),
+                              name='mean_factor')
+        q.normal(mean_width_factor.expand(num_factors,3,num_subjects),
+                               std_width_factor.expand(num_factors,3,num_subjects),
+                               name='width_factor')
         
         return q
+
 
 class HTFADecoder(nn.Module):
     """Generative Model for HTFA"""
@@ -56,50 +89,30 @@ class HTFADecoder(nn.Module):
         
         p = probtorch.Trace()
         
-        #second to outermost layer
+        #second from outermost layer
         mean_mean_weight = p.normal(self.hyper_mean_mean_mean_weight,
-                                    self.hyper_std_mean_mean_weight,
-                                   value=q['mean_mean_weight'],
-                                    name='mean_mean_weight')
+                                    self.hyper_std_mean_mean_weight)
         std_mean_weight = p.normal(self.hyper_mean_std_mean_weight,
-                                   self.hyper_std_std_mean_weight,
-                                  value=q['std_mean_weight'],
-                                  name='std_mean_weight')
+                                   self.hyper_std_std_mean_weight)
         mean_std_weight = p.normal(self.hyper_mean_mean_std_weight,
-                                  self.hyper_std_mean_std_weight,
-                                  value=q['mean_std_weight'],
-                                  name='mean_std_weight')
+                                  self.hyper_std_mean_std_weight)
         std_std_weight = p.normal(self.hyper_mean_std_std_weight,
-                                 self.hyper_std_std_std_weight,
-                                 value=q['std_std_weight'],
-                                 name='std_std_weight')
+                                 self.hyper_std_std_std_weight)
         
         mean_mean_factor = p.normal(self.hyper_mean_mean_mean_factor,
-                                   self.hyper_std_mean_mean_factor,
-                                   value=q['mean_mean_factor'],
-                                   name='mean_mean_factor')
+                                   self.hyper_std_mean_mean_factor)
         std_mean_factor = p.normal(self.hyper_mean_std_mean_factor,
-                                  self.hyper_std_std_mean_factor,
-                                  value=q['std_mean_factor'],
-                                  name='std_mean_factor')
+                                  self.hyper_std_std_mean_factor)
         mean_width_factor = p.normal(self.hyper_mean_mean_width_factor,
-                                  self.hyper_std_mean_width_factor,
-                                  value=q['mean_width_factor'],
-                                  name='mean_width_factor')
+                                  self.hyper_std_mean_width_factor)
         std_width_factor = p.normal(self.hyper_mean_std_width_factor,
-                                 self.hyper_std_std_width_factor,
-                                 value=q['std_width_factor'],
-                                 name='std_width_factor')
-        #third outermost layer
+                                 self.hyper_std_std_width_factor)
+        #third from outermost layer
         ## dimensions K,S
         mean_weight = p.normal(mean_mean_weight.expand(num_factors,num_subjects),
-                               std_mean_weight(num_factors,num_subjects),
-                               value=q['mean_weight'],
-                              name='mean_weight')
+                               std_mean_weight(num_factors,num_subjects))
         std_weight = p.normal(mean_std_weight.expand(num_factors,num_subjects),
-                             std_std_weight.expand(num_factors,num_subjects),
-                             value=q['std_weight'],
-                             name='std_weight')
+                             std_std_weight.expand(num_factors,num_subjects))
         ## dimensions K,3,S
         mean_factor = p.normal(mean_mean_factor.expand(num_factors,3,num_subjects),
                               std_mean_factor.expand(num_factors,3,num_subjects),
@@ -110,7 +123,7 @@ class HTFADecoder(nn.Module):
                                value=q['width_factor'],
                                name='width_factor')
         
-        #fourth outermost layer
+        #fourth from outermost layer
         ##dimensions N,K,S
         weights = p.normal(mean_weight.expand(num_times,num_factors,num_subjects),
                          std_weight.expand(num_times,num_factors,num_subjects),
