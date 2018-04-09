6e71f53caaa586ec4a5c6ef18ef5560696e70f40
Zulqarnain Khan <zqkhan>
2018-04-09 12:16:45 -0400

    create binary mean mask using fsl

---
+++ create_mean_images.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for file in /home/zulqarnain/Datasets/new_pieman/pieman_data_intact/*.nii
+do
+  fslmaths "$file" -Tmean -bin "${file}_mean"
+done
+
+fslmerge -t allmeanmasks4d /home/zulqarnain/Datasets/new_pieman/pieman_data_intact/*.nii.gz
+fslmaths allmeanmasks4d -Tmean propDatavox3d
+fslmaths propDatavox3d -thr 1 wholebrain
