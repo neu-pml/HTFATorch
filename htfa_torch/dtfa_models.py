46ede0d59be6a1b710c38ee8d664cb238a5a73e7
Eli Sennesh <e.sennesh@northeastern.edu>
2018-05-22 13:01:07 -0400

    dtfa_models.DeepTFAGuide: rewrite guide to do neural variational inference
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- htfa_torch/dtfa_models.py
+++ htfa_torch/dtfa_models.py
@@ -71,14 +71,15 @@ class DeepTFAGuideHyperparams(tfa_models.HyperParams):
                 'mu': torch.zeros(self.num_tasks, self.num_times,
                                   self.embedding_dim),
                 'sigma': torch.ones(self.num_tasks, self.num_times,
-                                    self.embedding_dim),
+                                    self.embedding_dim) *\
+                         tfa_models.SOURCE_WEIGHT_STD_DEV,
             },
             'template': {
-                'centers': {
+                'factor_centers': {
                     'mu': torch.zeros(self._num_factors, 3),
                     'sigma': torch.ones(self._num_factors, 3),
                 },
-                'log_widths': {
+                'factor_log_widths': {
                     'mu': torch.ones(self._num_factors),
                     'sigma': torch.ones(self._num_factors) *\
                              tfa_models.SOURCE_LOG_WIDTH_STD_DEV
@@ -90,36 +91,104 @@ class DeepTFAGuideHyperparams(tfa_models.HyperParams):
 
 class DeepTFAGuide(nn.Module):
     """Variational guide for deep topographic factor analysis"""
-    def __init__(self, num_factors, num_subjects, num_tasks, num_blocks=1,
-                 num_times=[1], embedding_dim=2):
+    def __init__(self, num_factors, block_subjects, block_tasks, num_blocks=1,
+                 num_times=[1], embedding_dim=2, hyper_means=None):
         super(self.__class__, self).__init__()
         self._num_blocks = num_blocks
         self._num_times = num_times
         self._num_factors = num_factors
+        self._embedding_dim = embedding_dim
 
+        self.block_subjects = block_subjects
+        self.block_tasks = block_tasks
+        num_subjects = len(set(self.block_subjects))
+        num_tasks = len(set(self.block_tasks))
+
+        self.htfa_template = htfa_models.HTFAGuideTemplatePrior()
         self.hyperparams = DeepTFAGuideHyperparams(self._num_blocks,
                                                    self._num_times,
                                                    self._num_factors,
                                                    num_subjects, num_tasks,
                                                    embedding_dim)
+        self.factors_embedding = nn.Sequential(
+            nn.Linear(self._embedding_dim, self._num_factors),
+            nn.Tanh(),
+            nn.Linear(self._num_factors, self._num_factors * 4),
+        )
+        self.weights_embedding = nn.Sequential(
+            nn.Linear(self._embedding_dim * 2, self._num_factors),
+            nn.Tanhshrink(),
+            nn.Linear(self._num_factors, self._num_factors),
+        )
 
-    def forward(self, trace, embedding, times=None, blocks=None,
+        self.epsilon = nn.Parameter(torch.Tensor([tfa_models.VOXEL_NOISE]))
+
+        if hyper_means is not None:
+            self.weights_embedding[-1].bias =\
+                nn.Parameter(hyper_means['weights'][0])
+            self.factors_embedding[-1].bias = nn.Parameter(torch.cat(
+                (hyper_means['factor_centers'],
+                 torch.Tensor([hyper_means['factor_log_widths']]).\
+                 expand(self._num_factors, 1)),
+                dim=1,
+            ).view(self._num_factors * 4))
+
+    def forward(self, trace, times=None, blocks=None,
                 num_particles=tfa_models.NUM_PARTICLES):
         params = self.hyperparams.state_vardict()
+        self.htfa_template(trace, params, num_particles=num_particles)
         for k, v in params.items():
             params[k] = v.expand(num_particles, *v.shape)
-
         if blocks is None:
             blocks = list(range(self._num_blocks))
-        weights = [b for b in blocks]
-        centers = [b for b in blocks]
-        log_widths = [b for b in blocks]
-        for (i, b) in enumerate(blocks):
-            weights[i], centers[i], log_widths[i] =\
-                embedding(trace, params, times=times, block=b,
-                          particles=True)
 
-        return weights, centers, log_widths
+        factor_weights = [None for b in blocks]
+        factor_centers = [None for b in blocks]
+        factor_log_widths = [None for b in blocks]
+
+        for (i, b) in enumerate(blocks):
+            subject = self.block_subjects[b]
+            task = self.block_tasks[b]
+            if times is None:
+                ts = (0, self._num_times[b])
+            else:
+                ts = times
+
+            if ('z^F_%d' % subject) not in trace:
+                factors_embed = trace.normal(params['factors']['mu'][:, subject, :],
+                                             params['factors']['sigma'][:, subject, :],
+                                             name='z^F_%d' % subject)
+            if ('z^P_%d' % subject) not in trace:
+                subject_embed = trace.normal(params['subject']['mu'][:, subject, :],
+                                             params['subject']['sigma'][:, subject, :],
+                                             name='z^P_%d' % subject)
+            if ('z^S_%dt%d_%d' % (task, ts[0], ts[1])) not in trace:
+                task_embed = trace.normal(
+                    params['task']['mu'][:, task, ts[0]:ts[1], :],
+                    params['task']['sigma'][:, task, ts[0]:ts[1], :],
+                    name='z^S_%dt%d_%d' % (task, ts[0], ts[1])
+                )
+
+            factor_params = self.factors_embedding(factors_embed)
+            factor_params = factor_params.view(-1, self._num_factors, 4)
+            subject_embed = utils.unsqueeze_and_expand(subject_embed, 1,
+                                                       ts[1]-ts[0], True)
+            weights_embed = torch.cat((subject_embed, task_embed), dim=-1)
+            weights = self.weights_embedding(weights_embed)
+
+            factor_weights[i] = trace.normal(
+                weights, self.epsilon[0],
+                name='Weights%dt%d-%d' % (b, times[0], times[1])
+            )
+            factor_centers[i] = trace.normal(factor_params[:, :, 0:3],
+                                             self.epsilon[0],
+                                             name='FactorCenters%d' % b)
+            factor_log_widths[i] = trace.normal(
+                factor_params[:, :, 3:].contiguous().view(-1, self._num_factors),
+                self.epsilon[0], name='FactorLogWidths%d' % b
+            )
+
+        return factor_weights, factor_centers, factor_log_widths
 
 class DeepTFAModel(nn.Module):
     """Generative model for deep topographic factor analysis"""
