270d4ee39e763e816d7b3e61c4312f5e1f4db92c
Eli Sennesh <sennesh.e@husky.neu.edu>
2019-01-14 18:18:47 -0500

    dtfa.DeepTFA: label reconstructions as "NeuralTFA" instead
    Signed-off-by: Eli Sennesh <sennesh.e@husky.neu.edu>

--- htfa_torch/dtfa.py
+++ htfa_torch/dtfa.py
@@ -483,7 +483,7 @@ class DeepTFA:
         plot = niplot.plot_glass_brain(
             image_slice, plot_abs=plot_abs, colorbar=True, symmetric_cbar=True,
             title=utils.title_brain_plot(block, self._blocks[block], labeler,
-                                         'Reconstruction'),
+                                         'NeuralTFA'),
             vmin=-self.activation_normalizers[block],
             vmax=self.activation_normalizers[block],
             **kwargs,
