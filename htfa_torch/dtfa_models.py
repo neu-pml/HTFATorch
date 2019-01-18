0541342a2b8593a830ecd3dbd800dc97f48b41a0
Eli Sennesh <esennesh@ccis.neu.edu>
2019-01-18 11:41:20 -0500

    dtfa_models.DeepTFADecoder: sample embeddings once per block/trial
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- htfa_torch/dtfa_models.py
+++ htfa_torch/dtfa_models.py
@@ -162,15 +162,17 @@ class DeepTFADecoder(nn.Module):
                              self._embedding_dim)
         origin = origin.to(params['subject']['mu'])
         if subject is not None:
-            subject_embed = self._predict_param(params, 'subject', subject,
-                                                None, 'z^P_%d' % subject, trace,
-                                                False, guide)
+            subject_embed = self._predict_param(
+                params, 'subject', subject, None,
+                'z^P_{%d,%d}' % (subject, block), trace, False, guide
+            )
         else:
             subject_embed = origin
         if task is not None:
-            task_embed = self._predict_param(params, 'task', task, None,
-                                             'z^S_%d' % task, trace, False,
-                                             guide)
+            task_embed = self._predict_param(
+                params, 'task', task, None, 'z^S_{%d,%d}' % (task, block),
+                trace, False, guide
+            )
         else:
             task_embed = origin
         factor_params = self.factors_embedding(subject_embed)
