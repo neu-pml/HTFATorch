842bb1ecf75608cbf07f5d360708b4ac912a3958
Eli Sennesh <sennesh.e@husky.neu.edu>
2019-01-23 18:22:07 -0500

    utils: pass loc=best to pyplot.legend
    Signed-off-by: Eli Sennesh <sennesh.e@husky.neu.edu>

--- htfa_torch/utils.py
+++ htfa_torch/utils.py
@@ -629,7 +629,7 @@ def uncertainty_palette(uncertainties, scalars=None, colormap='Set2'):
 def palette_legend(labels, colors):
     patches = [mpatches.Patch(color=colors[i], label=labels[i]) for i in
                range(len(colors))]
-    plt.legend(handles=patches)
+    plt.legend(handles=patches, loc='best')
 
 def isnan(tensor):
     # Gross: https://github.com/pytorch/pytorch/issues/4767
