4e385b4b97b8bec35a53a243eb515b2434d6bacb
esennesh <elisennesh@gmail.com>
2020-02-04 22:22:31 -0500

    dtfa.DeepTFA: pass num_particles to generative model
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- htfa_torch/dtfa.py
+++ htfa_torch/dtfa.py
@@ -218,7 +218,8 @@ class DeepTFA:
                     p = probtorch.Trace()
                     generative(decoder, p, times=trs, guide=q,
                                observations=activations, blocks=block_batch,
-                               locations=voxel_locations)
+                               locations=voxel_locations,
+                               num_particles=num_particles)
 
                     def block_rv_weight(node, prior=True):
                         result = 1.0
@@ -326,7 +327,8 @@ class DeepTFA:
                     p = probtorch.Trace()
                     generative(decoder, p, times=trs, guide=q,
                                observations=activations, blocks=block_batch,
-                               locations=voxel_locations)
+                               locations=voxel_locations,
+                               num_particles=num_particles)
 
                     _, ll, prior_kl = tfa.hierarchical_free_energy(
                         q, p, num_particles=num_particles
